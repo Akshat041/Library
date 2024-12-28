e66905cb0d95433fabf19ce5039125a85cbbd4ac
Akshat Singh <akshatofficial041@gmail.com>
2024-12-29 02:58:11 +0530

    Now I can correctly add book through modal.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 "use strict";
 
-const container = document.querySelector(".cards");
-
 const addBook = document.querySelector(".add-book");
-const dialog = document.querySelector("#modal");
+const modal = document.querySelector("#modal");
+const dialogForm = document.querySelector("#bookForm");
+
 const closeModal = document.querySelector(".close");
 const confirm = document.querySelector(".confirm");
 
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const pages = document.querySelector("#pages");
-const isRead = document.querySelector("#isRead");
+// const title = document.querySelector("#title");
+// const author = document.querySelector("#author");
+// const pages = document.querySelector("#pages");
+// const isRead = document.querySelector("#isRead");
 
 const myLibrary = [];
 
@@ -23,13 +23,62 @@ function Book(title, author, pages, isread) {
 }
 
 // creates original object and store that object in myLibrary.
-function addBookToLibrary(title, author, pages, isread) {
+function addBookToLibrary(title, author, pages, read) {
   // original object
-  const book = new Book(title, author, pages, isread);
+  const book = new Book(title, author, pages, read);
 
   myLibrary.push(book);
 
-  displayBook(book);
+  displayBook();
+}
+
+function createBookCard(book) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const title = document.createElement("h1");
+  title.classList.add("title");
+
+  const author = document.createElement("p");
+  author.classList.add("author");
+
+  const cardBottom = document.createElement("div");
+  cardBottom.classList.add("card-bottom");
+
+  const pages = document.createElement("p");
+  pages.classList.add("pages");
+
+  const bottomBtns = document.createElement("div");
+  bottomBtns.classList.add("bottom-btns");
+
+  const read = document.createElement("button");
+  read.classList.add("read");
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete");
+
+  title.textContent = `${book.title}`;
+  author.textContent = `${book.author}`;
+  pages.textContent = `${book.pages}`;
+  read.textContent = `${book.isread == true ? "Read" : "Not Read"}`;
+  deleteBtn.textContent = `Delete`;
+
+  bottomBtns.appendChild(read);
+  bottomBtns.appendChild(deleteBtn);
+  cardBottom.appendChild(pages);
+  cardBottom.appendChild(bottomBtns);
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(cardBottom);
+
+  return card;
+}
+
+function displayBook() {
+  const booksContainer = document.querySelector(".cards");
+
+  const newBookCard = createBookCard(myLibrary[myLibrary.length - 1]);
+  booksContainer.appendChild(newBookCard);
 }
 
 function clearInputs() {
@@ -41,40 +90,20 @@ function clearInputs() {
 
 addBook.addEventListener("click", () => {
   // event.preventDefault();
-  dialog.showModal();
+  modal.showModal();
 });
 
 confirm.addEventListener("click", () => {
-  addBookToLibrary(title.value, author.value, pages.value, isRead.value);
+  const Title = document.querySelector("#title");
+  const Author = document.querySelector("#author");
+  const Pages = document.querySelector("#pages");
+  const Read = document.querySelector("#isRead");
+
+  addBookToLibrary(Title.value, Author.value, Pages.value, Read.value);
+
   clearInputs();
 });
 
 closeModal.addEventListener("click", () => {
-  dialog.close();
+  modal.close();
 });
-
-// adding book and displaying
-// addBookToLibrary("Atomic habits", "maxwell", 123, true);
-// addBookToLibrary("A Village by the Sea ", "Gita Mehta", 123, true);
-// addBookToLibrary("A Call to Honour", "Jaswant Singh", 123, false);
-
-function displayBook(book) {
-  const card = document.createElement("div");
-  card.classList.add("card");
-
-  card.innerHTML = `<h1 class="title">${book.title}</h1>
-    <p class="author">- ${book.author}</p>
-    <div class="card-bottom">
-    <p class="pages">${book.pages} pages</p>
-    <div class="bottom-btns">
-    <button class="read">${
-      book.isread == true
-        ? (document.querySelector(".read").textContent = "Read")
-        : (document.querySelector(".read").textContent = "Not Read")
-    }</button>
-    <button class="delete-book">Delete</button>
-              </div>
-              </div>`;
-
-  container.appendChild(card);
-}
